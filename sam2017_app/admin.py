bc82fb5d2bc13afd34fc531290cff293f8cf60aa
hussienalrubaye <hussien89aa@yahoo.com>
2015-11-29 18:12:04 -0500

    add deadline for paper submitions and updating

--- sam2017_app/admin.py
+++ sam2017_app/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from sam2017_app.models import User, Paper, review, submission, notification
+from sam2017_app.models.paper import PaperDeadLine
 
 admin.site.register(User)
 admin.site.register(Paper)
 admin.site.register(review.Review)
 admin.site.register(submission.Submission)
 admin.site.register(notification.Notification)
+admin.site.register(PaperDeadLine)
 
 
 
