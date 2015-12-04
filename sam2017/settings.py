7696b289662c5e6b7f825c3d49e3c4a24ada1904
Chris Enoch <majorpayne327@gmail.com>
2015-12-04 13:43:24 -0500

    Fixed 404 on static files

--- sam2017/settings.py
+++ sam2017/settings.py
@@ -95,10 +95,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static/')
 
 STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(BASE_DIR), 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 BROKER_URL = 'django://'
