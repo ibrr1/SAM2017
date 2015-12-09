9b962f9b1cd4248e34701aa9035b872aff6979ec
Chris <cte6149@rit.edu>
2015-12-09 11:18:58 -0500

    Added Notifcations to submissions
    - Completed notifying PCC of Submission requirement
    - General Refactoring of the submission file
    - Added property to user called full_name

--- sam2017_app/models/user_model.py
+++ sam2017_app/models/user_model.py
@@ -18,6 +18,10 @@ class User(models.Model):
     type = models.CharField(max_length=10, choices=USER_TYPE, default=AUTHOR)
     admin = models.BooleanField(default=False)
 
+    @property
+    def full_name(self):
+        return "{0} {1}".format(self.first_name, self.last_name)
+
     @classmethod
     def create(cls, first_name, last_name, email, password):
         user = cls(first_name=first_name, last_name=last_name, email=email,
@@ -29,4 +33,4 @@ class User(models.Model):
         app_label = 'sam2017_app'
 
     def __str__(self):
-        return self.email
+        return "Name: {0} Email:{1}".format(self.full_name, self.email)
--- sam2017_app/views/paper_submission.py
+++ sam2017_app/views/paper_submission.py
@@ -1,21 +1,22 @@
-from time import timezone
+import datetime
+
+from django.contrib import messages
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from sam2017_app.models import paper
 from sam2017_app.models.user_model import User
 from sam2017_app.models import submission
 from sam2017_app.forms.paper_form import PaperSubmission
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from sam2017_app.views.notification import NotificationManager
 from sam2017_app.views.session import __is_session_open
 from sam2017_app.views.user_details import __add_general_content_to_context
-import datetime
-from django.contrib import messages
 
 
 def paper_submission(request):
-       ## check if  the submition date is passed so he cannot  publish one
-    last_month =  datetime.datetime.now() - datetime.timedelta(days=1)
-    PaperDeadLine =paper.PaperDeadLine.objects.filter(date_deadline__gte=last_month)
+    # check if  the submition date is passed so he cannot  publish one
+    last_month = datetime.datetime.now() - datetime.timedelta(days=1)
+    paper_dead_line = paper.PaperDeadLine.objects.filter(date_deadline__gte=last_month)
 
     if not __is_session_open(request):
         return HttpResponseRedirect('/')
@@ -31,11 +32,10 @@ def paper_submission(request):
 
     context.update(__add_general_content_to_context(user))
 
-    if not PaperDeadLine:
-         return render(request, 'paper_dead_line.html', context)
+    if not paper_dead_line:
+        return render(request, 'paper_dead_line.html', context)
 
     if request.method == "POST":
-
 
         paper_submission_form = PaperSubmission(request.POST, request.FILES)
         if paper_submission_form.is_valid():
@@ -45,13 +45,12 @@ def paper_submission(request):
                 context['error'] = error
             else:
                 df = datetime.datetime.now()
-                #file name is user last name + date + current time
-                filename = user.last_name + '_' + str(df.month)+"_"+ str(df.day)+"_"+ str(df.year)+ "_" +str(df.hour)+ "_"+ str(df.minute) +"_" +str(df.second) +"_"+ filename
+                # file name is user last name + date + current time
+                filename = user.last_name + '_' + str(df.month) + "_" + str(df.day) + "_" + str(df.year) + "_" + str(df.hour) + "_" + str(df.minute) + "_" + str(df.second) + "_" + filename
 
-                #save the file in a folder name submitted_papers in the project main folder
-                file_name='submitted_papers/'+ filename
-                handle_uploaded_file(request.FILES['paper'],file_name)
-                #----
+                # save the file in a folder name submitted_papers in the project main folder
+                file_name = 'submitted_papers/' + filename
+                handle_uploaded_file(request.FILES['paper'], file_name)
 
                 submitted_paper = paper.Paper()
                 submitted_paper.paper = filename
@@ -60,7 +59,7 @@ def paper_submission(request):
                 submitted_paper.author_contact = paper_submission_form.cleaned_data['author_contact']
                 submitted_paper.paper_format = paper_submission_form.cleaned_data['paper_format']
                 submitted_paper.date_created = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                submitted_paper.revision_paper= 1 if paper_submission_form.cleaned_data["is_this_a_revision_of_a_previously_submitted_paper"]=="Yes" else 0
+                submitted_paper.revision_paper = 1 if paper_submission_form.cleaned_data["is_this_a_revision_of_a_previously_submitted_paper"] == "Yes" else 0
 
                 submitted_paper.save()
                 
@@ -68,11 +67,14 @@ def paper_submission(request):
                 new_submission.paper = submitted_paper
                 new_submission.submitter = user
 
-
-
                 new_submission.save()
 
-
+                try:
+                    pcc = User.objects.get(type=User.PCC)
+                    nm = NotificationManager.create()
+                    nm.send_notification(recipients=[pcc], message="{0} has submitted a paper with the title {1}".format(user.full_name, submitted_paper.title))
+                except:
+                    print("Something went wrong when adding the task")
 
                 messages.success(request, 'Thanks, You have successfully submitted a paper')
 
@@ -80,9 +82,8 @@ def paper_submission(request):
 
     return render(request, 'paper_submission.html', context)
 
-# End paper submission
 
 def handle_uploaded_file(f, file_name):
-    with open( file_name , 'wb+') as destination:
+    with open(file_name, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
